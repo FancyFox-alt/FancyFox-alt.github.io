678974722eb83c391064b79aed12170e0f9f4e19
FancyFox-alt <83415495+FancyFox-alt@users.noreply.github.com>
2021-05-03 16:57:24 -0400

    Create Perroquet.js

---
+++ Perroquet.js
@@ -0,0 +1,11 @@
+var Colour= prompt("Quelle est ta couleur préférée ?");
+ alert("Eh!  Ma couleur préférée est aussi " + Colour + " Eh!");
+ 
+ var Jeux= prompt("Quelle est ta jeux préférée ?");
+ alert("Eh!  Ma jeux préférée est aussi " + Jeux + " Eh!");
+ 
+ var Livre= prompt("Quelle est ta livre préférée ?");
+ alert("Eh!  Ma livre préférée est aussi " + Livre + " Eh!");
+ 
+ var Film= prompt("Quelle est ta film préférée ?");
+ alert("Eh!  Ma film préférée est aussi " + Film + " Eh!");
