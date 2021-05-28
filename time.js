c6e5115b06e0b20d463f9178c50b579303685648
FancyFox-alt <83415495+FancyFox-alt@users.noreply.github.com>
2021-05-28 10:58:24 -0400

    Update time.js

--- time.js
+++ time.js
@@ -1,16 +1,30 @@
-//finding the hour
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Zodiac</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+ 
+  </head>
+  <body>
+  <script>
+      //finding the hour
 //const hr = 15
 var hr = new Date();
 hr = hr.getHours();
 //Responses
 if ((hr >=2) && (hr <= 10)){
-  alert ("Bon matin!")
+  alert ("Bon matin!");
 }else if ((hr >=11) && (hr <= 15)){
-  alert ("Bonjour!")
+  alert ("Bonjour!");
 }else if ((hr >=16) && (hr <= 19)){
-  alert ("Bonne soirée!")
+  alert ("Bonne soirée!");
 }else if ((hr >=20) && (hr <= 23)){
-  alert ("Bonne nuit!")
+  alert ("Bonne nuit!");
 } else {
-alert("Tu devrais être au lit! ")
-}
+alert("Tu devrais être au lit! ");
+} 
+</script>
+  </body>
+</html>
